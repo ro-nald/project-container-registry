f4e9430c29ef5d8d1f45cb2df5ae187ca2be36c2
ro-nald <daddsronald@gmail.com>
2025-08-28 23:54:29 +0100

    Extracted providers configuration

--- main.tf
+++ main.tf
@@ -1,27 +1,7 @@
-terraform {
-    required_providers {
-      azurerm = {
-        source = "hashicorp/azurerm"
-        version = "4.38"
-      }
-    }
-    required_version = "1.13.0"
+module "resource_collection" {
+  source = "resource-collection"
 }
 
-provider "azurerm" {
-  subscription_id = "c45ffaa7-f104-4c50-baee-2a8499e1eb45"
-  features {}
-}
-
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
-}
-
-resource "azurerm_container_registry" "acr" {
-  name                = "containerRegistry1"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  sku                 = "Basic"
-  admin_enabled       = false
+module "container_registry" {
+  source = "container-registry"
 }
